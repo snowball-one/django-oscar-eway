6c9e493964bac8b4d24b5fcd0c7e61d1aa4fccae
Sebastian Vetter <sebastian@roadside-developer.com>
2012-11-10 12:09:17 +1100

    Update bank card form to use oscar form fields

--- eway/forms.py
+++ eway/forms.py
@@ -1,20 +1,64 @@
+from datetime import date
+
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from oscar.apps.payment import forms
+
+
+def expiry_month_choices(self):
+        return [("%.2d" % x, "%.2d" % x) for x in xrange(1, 13)]
+
+
+def expiry_year_choices(self, num_years=5):
+    return [(x, x) for x in xrange(date.today().year, date.today().year + num_years)]
+
+
+def start_month_choices(self):
+    months = [("%.2d" % x, "%.2d" % x) for x in xrange(1, 13)]
+    months.insert(0, ("", "--"))
+    return months
+
+
+def start_year_choices(self, num_years=5):
+    years = [(x, x) for x in xrange(date.today().year - num_years, date.today().year + 1)]
+    years.insert(0, ("", "--"))
+    return years
+
 
 class EwayBankcardForm(forms.Form):
-    token_customer_id = forms.ChoiceField(required=False, label=_("Existing cards"))
-
+    token_customer_id = forms.ChoiceField(required=False, label=_("Existing cards")) 
     EWAY_CARDNAME = forms.CharField(label=_("Card type"))
-    EWAY_CARDNUMBER = forms.CharField(label=_("Card number"))
-    EWAY_CARDEXPIRYMONTH = forms.CharField(label=_("Expiry month"))
-    EWAY_CARDEXPIRYYEAR = forms.CharField(label=_("Expiry year"))
+    EWAY_CARDNUMBER = forms.BankcardNumberField(
+        max_length=20,
+        widget=forms.TextInput(attrs={'autocomplete':'off'}),
+        label=_("Card number")
+    )
+    EWAY_CARDCVN = forms.RegexField(
+        required=True,
+        label=_("CVV Number"),
+        regex=r'^\d{3,4}$',
+        widget=forms.TextInput(attrs={'size': '5'}),
+        help_text=_("This is the 3 or 4 digit security number on the back of your bankcard")
+    )
 
-    EWAY_CARDSTARTMONTH = forms.CharField(
+    EWAY_CARDEXPIRYMONTH = forms.ChoiceField(
+        choices=expiry_month_choices(),
+        label=_("Expiry month")
+    )
+    EWAY_CARDEXPIRYYEAR = forms.ChoiceField(
+        choices=expiry_year_choices(5),
+        label=_("Expiry year")
+    )
+
+    # these fields are used in the UK only
+    EWAY_CARDSTARTMONTH = forms.ChoiceField(
+        choices=start_month_choices(),
         widget=forms.HiddenInput(),
         required=False
     )
-    EWAY_CARDENDMONTH = forms.CharField(
+    EWAY_CARDSTARTYEAR = forms.ChoiceField(
+        choices=start_year_choices(),
         widget=forms.HiddenInput(),
         required=False
     )
@@ -22,8 +66,6 @@ class EwayBankcardForm(forms.Form):
         widget=forms.HiddenInput(),
         required=False
     )
-
-    EWAY_CARDCVN = forms.CharField(label=_("CVN"))
 
 
 class EwayHiddenBankcardForm(EwayBankcardForm):
